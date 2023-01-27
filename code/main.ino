aedb24a3984e7e8cd28f02fa4f07c46e25ba8510
Thomas <mail@lokalnetz.com>
2023-01-27 20:57:57 +0100

    add fun with chasing leds using encoder

--- code/main.ino
+++ code/main.ino
@@ -1,59 +1,100 @@
+#include "pattern.h";
 #include <RTClib.h>;
 
-#define SHCP 8  // SHIFT
-#define STCP 9  // LATCH
-#define DATA 10 // DS
+#define OE    7 // OE
+#define DATA  8 // DS
+#define STCP  9 // LATCH
+#define SHCP 10 // SHIFT
 
-uint32_t countTime    = 0;
-uint32_t countPattern = 0;
-bool debug            = 0;
+#define SW    1 // ENCODER SWITCH
+#define CLK   2 // ENCODER CLOCK
+#define DT    3 // ENCODER DATA
 
-RTC_DS1307 rtc;
+RTC_DS1307 RTC;
+
+int pattern = 0;
+int encPos  = 0;
+int encCurrentState;
+int encLastState;
 
 void setup() {
-
-    if (debug) Serial.begin(9600);
 
     pinMode(STCP, OUTPUT);
     pinMode(SHCP, OUTPUT);
     pinMode(DATA, OUTPUT);
+    pinMode(OE, OUTPUT);
 
-    // RTC
-    pinMode(A3, OUTPUT);
-    digitalWrite(A3, HIGH);
-    pinMode(A2, OUTPUT);
-    digitalWrite(A2, LOW);
+    // ENABLE IF THERE IS NO EXTERNAL RESISTOR
+    // pinMode(CLK, INPUT_PULLUP);
+    // pinMode(DT, INPUT_PULLUP);
 
-    if (!rtc.begin()) {
+    // ENCODER INTERRUPT LISTENER
+    encLastState = digitalRead(CLK);
+    // PCB NOT SUPPORTED IN THIS WAY ON A ATTINY
+    // attachInterrupt(0, updateEncoder, CHANGE);
+    // attachInterrupt(1, updateEncoder, CHANGE);
+
+    if (!RTC.begin()) {
         while (1)
             delay(10);
     }
 
-    if (!rtc.isrunning()) {
+    if (!RTC.isrunning()) {
         // INITIAL RTC DEVICE
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
+
 }
 
 void loop() {
 
-    DateTime now = rtc.now();
+    DateTime now = RTC.now();
 
-    char format[6] = "hhmmss";
+    char formatTime[6] = "hhmmss";
+    char formatDate[4] = "DDMM";
 
-    countTime = strtoul(now.toString(format), NULL, 10);
+    updateEncoder();
 
-    digitalWrite(STCP, LOW);
+    switch (encPos) {
+    case 1:
+        if (delayTime(250)) {
+            uint32_t ran = getRandomBits(24);
+            digitalWrite(STCP, LOW);
+            shiftOut(DATA, SHCP, MSBFIRST, ran);
+            shiftOut(DATA, SHCP, MSBFIRST, (ran >> 8));
+            shiftOut(DATA, SHCP, MSBFIRST, (ran >> 16));
+            digitalWrite(STCP, HIGH);
+        }
 
-    shiftOut(DATA, SHCP, MSBFIRST, (countTime >> 16));
-    shiftOut(DATA, SHCP, MSBFIRST, (countTime >> 8));
-    shiftOut(DATA, SHCP, MSBFIRST, countTime);
-    shiftOut(DATA, SHCP, MSBFIRST, 0);
+        break;
+    case 2:
+        digitalWrite(STCP, LOW);
+        shiftOut(DATA, SHCP, MSBFIRST, knight[pattern][2]);
+        shiftOut(DATA, SHCP, MSBFIRST, knight[pattern][1]);
+        shiftOut(DATA, SHCP, MSBFIRST, knight[pattern][0]);
+        digitalWrite(STCP, HIGH);
 
-    digitalWrite(STCP, HIGH);
+        if(delayTime(50)) pattern = (pattern + 1) % 49;
 
-    if (debug) {
-        serialMonitor(now.hour(), now.minute(), now.second(), countTime);
-        delay(1000);
+        break;
+    case 3:
+        digitalWrite(STCP, LOW);
+        shiftOut(DATA, SHCP, MSBFIRST, police[pattern][2]);
+        shiftOut(DATA, SHCP, MSBFIRST, police[pattern][1]);
+        shiftOut(DATA, SHCP, MSBFIRST, police[pattern][0]);
+        digitalWrite(STCP, HIGH);
+
+        if (delayTime(100)) pattern = (pattern + 1) % 8;
+
+        break;
+    default:
+        uint32_t currentTime = strtoul(now.toString(formatTime), NULL, 10);
+        digitalWrite(STCP, LOW);
+        shiftOut(DATA, SHCP, MSBFIRST, currentTime);
+        shiftOut(DATA, SHCP, MSBFIRST, (currentTime >> 8));
+        shiftOut(DATA, SHCP, MSBFIRST, (currentTime >> 16));
+        digitalWrite(STCP, HIGH);
+
+        break;
     }
 }
